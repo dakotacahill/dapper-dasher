ebe8c8c18e46e9fff63cc1fa6626092b10e0a5c3
Dakota Cahill <dakota.cahill@protonmail.com>
2024-08-01 05:45:03 -0400

    Drew in sprites, addeded delta time and anims

--- dasher.cpp
+++ dasher.cpp
@@ -7,30 +7,40 @@ int main()
     const int windowHeight{380};
     InitWindow( windowWidth, windowHeight, "Dapper Dasher");
 
-    // acceleration due to gravity (pixels/frame)/frame
-    const int gravity{1};
+    // acceleration due to gravity (pixels/s)/s
+    const int gravity{1'000};
 
-    // rectanglke dimensions 
-    const int width{50};
-    const int height{80};
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    Rectangle scarfyRec;
+    scarfyRec.width = scarfy.width/6;
+    scarfyRec.height = scarfy.height;
+    scarfyRec.x = 0;
+    scarfyRec.y = 0;
+    Vector2 scarfyPos;
+    scarfyPos.x  = windowWidth/2 - scarfyRec.width/2;
+    scarfyPos.y  = windowHeight - scarfyRec.height;
+    
+    int frame{}; // animation frame
+    const float updateTime{1.0 / 12.0}; // amt of time before we update anim frame
+    float runningTime{};
 
-    // is the rectangle in the air
-    bool isInAir{};
-    // jump velocity
-    const int jumpVel{-22};
+    bool isInAir{}; // is the rectangle in the air
+    const int jumpVel{-600}; // jump velocity (pixels/s)
 
-    int posY{windowHeight - height};
     int velocity{0}; 
 
     SetTargetFPS(60);
     while ( !WindowShouldClose() )
     {
+        // delta time (time since last frame)
+        const float dT{GetFrameTime()};
+
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
 
         // perform ground check
-        if (posY > windowHeight - height)
+        if (scarfyPos.y > windowHeight - scarfyRec.height)
         {
             velocity = 0;
             isInAir = false;
@@ -39,7 +49,7 @@ int main()
         {
             // rectangle is in the air
             // apply gravity
-            velocity += gravity;
+            velocity += gravity * dT;
             isInAir = true;
         }
 
@@ -50,13 +60,28 @@ int main()
         }
 
         // update position
-        posY += velocity;
-         
-        
-        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+        scarfyPos.y += velocity * dT;
+
+        // update running time
+        runningTime += dT;
+
+        if (runningTime >= updateTime)
+        {
+            runningTime = 0;
+            // update animation frame
+            scarfyRec.x = frame * scarfyRec.width;
+            frame++;
+            if (frame > 5)
+            {
+                frame = 0;
+            }
+        }
+
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE); 
 
         // stop drawing
         EndDrawing();
     }
+    UnloadTexture(scarfy);
     CloseWindow();
 }
